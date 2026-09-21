d30dcce2d2229882a9a3bfc9ff6c815567e758d2
Dmitry Rostovtsev <ceremoneymeister@gmail.com>
2026-09-21 07:46:18 +0000

    chore(release): freeze Korra 0.21.11

--- korra_cli/__init__.py
+++ korra_cli/__init__.py
@@ -14,8 +14,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.21.10"
-__release_date__ = "2026.9.18"
+__version__ = "0.21.11"
+__release_date__ = "2026.9.21"
 
 
 def _ensure_utf8():
